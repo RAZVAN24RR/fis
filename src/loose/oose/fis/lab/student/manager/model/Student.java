7a355e68a3c302d071eeba25c1854aa35e9bcb18
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-03-19 17:04:00 +0200

    Modify the Student Model

--- src/loose/oose/fis/lab/student/manager/model/Student.java
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -3,73 +3,92 @@ package loose.oose.fis.lab.student.manager.model;
 import java.util.Objects;
 
 public class Student {
-    private String firstName;
-    private String lastName;
-    private int age;
-    private double meanGrade;
 
-    public Student(String firstName, String lastName, int age, double meanGrade) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.meanGrade = meanGrade;
-    }
+  private String firstName;
+  private String lastName;
+  private int age;
+  private double meanGrade;
+  private double cateRestanteAre;
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", meanGrade=" + meanGrade +
-                '}';
-    }
+  public Student(
+    String firstName,
+    String lastName,
+    int age,
+    double meanGrade,
+    double restante
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.meanGrade = meanGrade;
+    this.cateRestanteAre = restante;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return age == student.age &&
-                Double.compare(student.meanGrade, meanGrade) == 0 &&
-                Objects.equals(firstName, student.firstName) &&
-                Objects.equals(lastName, student.lastName);
-    }
+  @Override
+  public String toString() {
+    return (
+      "Student{" +
+      "firstName='" +
+      firstName +
+      '\'' +
+      ", lastName='" +
+      lastName +
+      '\'' +
+      ", age=" +
+      age +
+      ", meanGrade=" +
+      meanGrade +
+      '}'
+    );
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, age, meanGrade);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Student student = (Student) o;
+    return (
+      age == student.age &&
+      Double.compare(student.meanGrade, meanGrade) == 0 &&
+      Objects.equals(firstName, student.firstName) &&
+      Objects.equals(lastName, student.lastName)
+    );
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName, age, meanGrade);
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public double getMeanGrade() {
-        return meanGrade;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public void setMeanGrade(double meanGrade) {
-        this.meanGrade = meanGrade;
-    }
+  public double getMeanGrade() {
+    return meanGrade;
+  }
+
+  public void setMeanGrade(double meanGrade) {
+    this.meanGrade = meanGrade;
+  }
 }
